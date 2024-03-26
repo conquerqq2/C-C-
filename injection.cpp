fc0469960162396d54ab96f05d948d13b3800c9a
conquerqq2 <142661811+conquerqq2@users.noreply.github.com>
2024-03-26 15:27:00 +0800

    Update injection.cpp

--- injection.cpp
+++ injection.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 struct Injection {
     double dosage;
+    int times;
     double singleDosage;
 };
 
@@ -12,12 +13,15 @@ void generateCombinations(vector<Injection>& combinations) {
     double dosages[] = {0.8, 2, 3, 4, 5};
     int numDosages = sizeof(dosages) / sizeof(dosages[0]);
     int numTimes = 10;
+    int maxTimes = 180;
+    int increment = 10;
 
     for (int i = 0; i < numDosages; ++i) {
-        for (int j = 0; j < numTimes; ++j) {
+        for (int j = minTimes; j <= maxTimes; j += increment) {
             Injection injection;
             injection.dosage = dosages[i];
-            injection.singleDosage = dosages[i] / (j + 1);
+            injection.times = j;
+            injection.singleDosage = dosages[i] / j;
             combinations.push_back(injection);
         }
     }
@@ -30,7 +34,7 @@ int main() {
     // 输出量组合和单剂量
     cout << "Combination\tSingle Dosage (ml)" << endl;
     for (int i = 0; i < combinations.size(); ++i) {
-        cout << "Dosage: " << combinations[i].dosage << "ml, Times: " << (i % 10) + 10 << "\t" << combinations[i].singleDosage << "ml" << endl;
+        cout << "Dosage: " << combinations[i].dosage << "ml, Times: " << combinations[i].times << "\t" << combinations[i].singleDosage << "ml" << endl;
     }
 
     return 0;
